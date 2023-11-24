337aab2917dc42db6ae61e329bf7dcefbb7488c0
barochiarg <Rishitkumar.Barochia@ibm.com>
2023-11-24 22:36:13 +0530

    add store-secrets script

---
+++ cp4i-cfn/scripts/store-secrets.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+
+function validate_cmd_options() {
+
+    # validate base_path
+    if [ -z $base_path ]; then
+        base_path=$(pwd)
+    fi
+
+    echo "base_path.."$base_path
+    echo "***** All arguments are validated *****"
+}
+
+# extract_login_cred
+function extract_login_cred() {
+    credfile=$(cat $cred_path)
+    
+    # read cluster_url from .cred
+    if [ -z $cluster_url ]; then
+      cluster_url=$(echo "$credfile" | grep -o 'https://[^ ]*')
+    fi
+    
+    # read cluster_username from .cred
+    if [ -z $cluster_username ]; then
+      cluster_username=$(echo "$credfile" | grep -oE -- '--username ([^ ]+)' | cut -d' ' -f2)
+    fi
+
+    # read cluster_password from .cred
+    if [ -z $cluster_password ]; then
+      cluster_password=$(echo "$credfile" | grep -oE -- '--password ([^ ]+)' | cut -d' ' -f2)
+    fi
+    
+    # read region from ec2 metadata
+    if [ -z $region ]; then
+      region=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
+    fi
+
+    echo "cluster_url: $cluster_url, cluster_username: $cluster_username, cluster_password: $cluster_password, subnets: $subnets"
+
+    aws_region=$region
+    
+    echo "***** OC credentials are extracted *****"
+}
+
+# oc login
+ocloginmaxcounter=5
+oclogincounter=0
+ocloginfailed=false
+function oc_login() {
+    echo "oc login $cluster_url --username=$cluster_username --password=$cluster_password --insecure-skip-tls-verify"
+    oc login $cluster_url --username=$cluster_username --password=$cluster_password --insecure-skip-tls-verify
+
+    if [ $? == 0 ]; then
+        echo "***** OC login is successful *****"
+        ocloginfailed=false
+    else
+      while [ $oclogincounter -lt $oclogincounter ];
+      do
+          echo "oc login failed..!!$oclogincounter"
+          sleep 30
+          oc_login
+          ocloginfailed=true
+          ((oclogincounter++))
+      done
+    fi
+   
+    echo "ocloginfailed..$ocloginfailed"
+    if $ocloginfailed; then
+      echo "***** OC login is failed after $ocloginmaxcounter attempt *****"
+      exit 1;
+    fi
+}
+
+
+SHORT=bp:,h
+LONG=base-path:,help
+OPTS=$(getopt -a -n weather --options $SHORT --longoptions $LONG -- "$@")
+
+eval set -- "$OPTS"
+
+while :
+do
+  case "$1" in
+    -bp | --base-path )
+      base_path="$2"
+      shift 2
+      ;;
+
+    -h | --help)
+      "This is a store-secrets script"
+      exit 2
+      ;;
+    --)
+      shift;
+      break
+      ;;
+    *)
+      echo "Invalid option: $1"
+      ;;
+  esac
+done
+
+validate_cmd_options
+
+# create installer files
+mkdir -p $base_path/installer-files
+
+export installer_workspace=$base_path/installer-files
+export cred_path=$installer_workspace/.cred
+
+# extract login credential
+extract_login_cred
+
+# oc login
+oc_login
+
+openshift_url=$(oc get route --all-namespaces | grep console-openshift | awk '{print $3}')
+aws secretsmanager put-secret-value --secret-id "$clustername-OpenshiftURL" --secret-string "https://$openshift_url"
+
+rosa_api=$(awk '/https/{print $3}' $cred_path)
+aws secretsmanager put-secret-value --secret-id "$clustername-ROSAAPI" --secret-string "$rosa_api"
+
+rosa_username=$(awk '/https/{print $5}' $cred_path)
+aws secretsmanager put-secret-value --secret-id "$clustername-Openshift-Username" --secret-string "$rosa_username"
+
+rosa_pwd=$(awk '/https/{print $7}' $cred_path)
+aws secretsmanager put-secret-value --secret-id "$clustername-Openshift-Password" --secret-string "$rosa_pwd"
+
+rosa_login_command=$(awk '/https/' $cred_path)
+aws secretsmanager put-secret-value --secret-id "$clustername-cluster-login-command" --secret-string "$rosa_login_command"
+
+echo "***** rosa cluster secrets are stored  *****"
